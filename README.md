e8c685d3f92194d5afc4ef49deeafe8278c94b92
obdeijn <arnold.obdeijn@gmail.com>
2021-05-28 11:19:43 +0200

    Add exporting and importing of groups and associated realm roles. Update README

--- README.md
+++ README.md
@@ -3,27 +3,34 @@ Automation tooling for [Keycloak](https://www.keycloak.org/)
 
 ## Introduction
 This repository provides some tooling to automate various tasks for Keycloak. Still in alpha state. Currently there is support for:
-- exporting users and the groups they belong to
-- processing an input file with users and the groups they should belong to and configure a Keycloak realm accordingly
+- importing and exporting group membership configuration
+- importing and exporting groups and associated realm roles
 
-**Note**: the only thing the script does is add (or remove) existing users to (or from) existing groups. It does not create any users or groups.
+**Note**: the users in the group membership configuration should be existing users. Non existing users are skipped, with a warning.
 
 ## Usage
 ```python
-# Process an input csv file of users and groups and add/remove users to/from groups accordingly.
+# Process an input csv file of desired user group memberships
+# and add/remove users to/from groups accordingly.
 python admin.py execute -i <inputfile>
 
-# Process an input csv file of users and groups, in check mode
+# The same as above, but in check mode, do not actually change anything
 python admin.py execute -i <inputfile> -c
 
-# Export users and the groups they belong to as csv (only usernames)
+# Export users and the groups they are a member of as csv (only usernames)
 python admin.py exportcsv
 
-# Export the users in a group (only usernames)
+# Export the members of a group (only usernames)
 python admin.py exportgroup -n <group name>
+
+# Export groups and associated realm roles as a yaml file
+python admin.py export_groups_and_roles -o <outputfile>
+
+# Import groups and associated realm roles from a yaml file
+python admin.py import_groups_and_roles -i <inputfile>
 ```
 
-### Structure of the csv file:
+### Structure of the group memberships csv file:
 ```csv
 username, groups
 user1, group_a;group_b
@@ -34,4 +41,4 @@ etc..
 ```
 
 ## Configuration
-The script expects an url to your Keycloak installation, a realm and credentials of a user with realm admin privileges. Currently these need to be configured as env vars, see env_example.sh.
+The module expects a url to your Keycloak installation, a realm and credentials of a user with realm admin privileges. Currently these need to be configured as env vars, see env_example.sh.
--- src/admin.py
+++ src/admin.py
@@ -1,10 +1,13 @@
+import os
 import sys
 import argparse
 import logging
 import csv
+import yaml
 
 from keycloak import KeycloakAdmin
-from settings import kc_base_url, kc_realm, kc_username, kc_password 
+from keycloak.exceptions import KeycloakGetError
+from settings import kc_base_url, kc_realm, kc_username, kc_password
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,17 +27,21 @@ class AuthorizationManager:
         )
 
     def get_group_lookup(self):
+        """ Create a dictionary that maps group name to group id """
         if not self._group_lookup:
             groups = self.admin.get_groups()
             self._group_lookup = dict()
             for group in groups:
                 logger.info(f'group name: {group["name"]}')
-                logger.info(f'group path: {group["path"]}')
                 self._group_lookup[group['name']] = group['id']
         return self._group_lookup
 
     def get_users_current_state(self, group_lookup):
-        users_current = {}
+        """
+        Create a dictionary thats maps username to a set of
+        the names of the groups that the user is currently a member of
+        """
+        users_current = dict()
 
         for group_name in group_lookup:
             group_id = group_lookup[group_name]
@@ -50,6 +57,10 @@ class AuthorizationManager:
         return users_current
 
     def get_users_desired_state(self, path_to_csv):
+        """
+        Read input from csv and output a dictionary that maps username to a set of
+        the names of the groups that the user is a member of
+        """
         users_desired = {}
         with open(path_to_csv) as csv_file:
             csv_reader = csv.DictReader(csv_file, skipinitialspace=True, delimiter=',')
@@ -58,66 +69,91 @@ class AuthorizationManager:
         return users_desired
 
     def export_users_current_csv(self, users_current):
-        """ Export users with csv format:
+        """
+        Export usernames and the groups that the user is a member of, in csv format:
         user001, group001;group002
         """
         print('username, groups')
-        for user in users_current:
+        users_current_sorted = dict(sorted(users_current.items()))
+        for user in users_current_sorted:
             groups = ';'.join(users_current[user])
             print(f'{user}, {groups}')
 
-    def export_group_csv(self, group_name):
+    def export_groups_and_roles(self, outputfile):
+        """
+        Export the groups and the realm roles attached to the group as a yaml file.
+        This only exports the groups and roles as such, not the members of the group.
+        """
+        export = {'groups': []}
+        groups = self.admin.get_groups()
+        for group in groups:
+            group['roles'] = self.admin.get_group_realm_roles(group['id'])
+            export['groups'].append(group)
+        if outputfile:
+            with open(outputfile, 'w') as file:
+                yaml.dump(export, file)
+        else:
+            yaml.dump(export, sys.stdout)
+
+    def import_groups_and_roles(self, inputfile):
+        """
+        Import the groups and the realm roles attached to the group from a yaml file.
+        This only imports groups and roles as such, not the members of the group.
+        """
+        with open(inputfile) as f:
+            groups_data = yaml.load(f, Loader=yaml.FullLoader)
+        for group in groups_data['groups']:
+            realm_roles = []
+            for role in group['roles']:
+                role_repr = self.create_role_if_not_exists(role)
+                realm_roles.append(role_repr)
+            del group['id']
+            del group['roles']
+            self.admin.create_group(group, None, True)
+            group_id = self.get_group_id_by_group_name(group['name'])
+            self.admin.assign_group_realm_roles(group_id, realm_roles)
+
+    def create_role_if_not_exists(self, role):
+        """
+        Create a realm role if it does not exist yet
+        """
+        try:
+            role_repr = self.admin.get_realm_role(role['name'])
+        except KeycloakGetError as e:
+            # sketchy error handling.. create role if it doesn't exist
+            if '404' in str(e):
+                del role['containerId']
+                del role['id']
+                self.admin.create_realm_role(role)
+                role_repr = self.admin.get_realm_role(role['name'])
+        return role_repr
+
+    def get_group_id_by_group_name(self, group_name):
         group_path = f'/{group_name}'
         group = self.admin.get_group_by_path(group_path)
         if not group:
             logger.warning(f'Group {group_name} not found')
             return False
+        return group['id']
 
-        group_id = group['id']
+    def export_group_members(self, group_name):
+        """
+        Make an export of all members of a group (username only)
+        """
+        group_id = self.get_group_id_by_group_name(group_name)
         members = self.admin.get_group_members(group_id)
-        print(group['name'])
+        print(group_name)
         for member in members:
             username = member['username']
             print(f'- {username}')
 
-    def export_users_current_yaml(self, users_current):
-        """ Export users with yaml format:
-        users:
-          - username: user001
-             groups:
-               - group001
-               - group002
-        """
-        print('users:')
-        for user in users_current:
-            print(f'  - username: {user}:')
-            print(f'    groups:')
-            for group in users_current[user]:
-                print(f'      - {group}')
-
     def get_user_groups(self, username, state_lookup):
+        """
+        Return the set of groups that user is a member of from the lookup
+        """
         if username not in state_lookup:
             return set()
         return state_lookup[username]
-
-    def add_user_to_group(self, user_id, group_id):
-        if not self.checkmode:
-            self.admin.group_user_add(user_id, group_id)
-
-    def remove_user_from_group(self, user_id, group_id):
-        if not self.checkmode:
-            self.admin.group_user_remove(user_id, group_id)
-
-    def remove_user_from_groups(self, user, remove_from_groups):
-        username = user['username']
-        for group_name in remove_from_groups:
-            logger.info(f'Remove {username} from group {group_name}')
-            try:
-                group_id = self.get_group_lookup()[group_name]
-            except KeyError:
-                logger.warning(f'Group unknown: {group_name}')
-            else:
-                self.remove_user_from_group(user['id'], group_id)
 
     def add_user_to_groups(self, user, add_to_groups):
         username = user['username']
@@ -130,7 +166,30 @@ class AuthorizationManager:
             else:
                 self.add_user_to_group(user['id'], group_id)
 
+    def remove_user_from_groups(self, user, remove_from_groups):
+        username = user['username']
+        for group_name in remove_from_groups:
+            logger.info(f'Remove {username} from group {group_name}')
+            try:
+                group_id = self.get_group_lookup()[group_name]
+            except KeyError:
+                logger.warning(f'Group unknown: {group_name}')
+            else:
+                self.remove_user_from_group(user['id'], group_id)
+
+    def add_user_to_group(self, user_id, group_id):
+        if not self.checkmode:
+            self.admin.group_user_add(user_id, group_id)
+
+    def remove_user_from_group(self, user_id, group_id):
+        if not self.checkmode:
+            self.admin.group_user_remove(user_id, group_id)
+
     def get_to_desired_state(self, path_to_csv):
+        """
+        Read in a csv with desired user group memberships.
+        Compare to the current state and change it to the desired state
+        """
         logger.info(f'Get user groups in desired state')
         logger.info(f'Base url: {kc_base_url}')
         logger.info(f'Realm: {kc_realm}')
@@ -155,7 +214,11 @@ class AuthorizationManager:
             
             remove_from_groups = current_groups - desired_groups
             add_to_groups = desired_groups - current_groups
-    
+            if not remove_from_groups and not add_to_groups:
+                # Nothing to do for this user
+                continue
+
+            logger.info(f'Get user id for {username}')
             user_id = self.admin.get_user_id(username)
             if user_id is None:
                 logger.warning(f'User unknown: {username}')
@@ -173,17 +236,31 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('cmd', help='Command to execute')
     parser.add_argument('-i', '--inputfile', help='Input file to process')
+    parser.add_argument('-o', '--outputfile', help='Output file to write to')
     parser.add_argument('-c', '--checkmode', help='Run in check mode', action='store_true')
     parser.add_argument('-n', '--name', help='Name of entity')
     args = parser.parse_args()
     cmd = args.cmd
     inputfile = args.inputfile
+    outputfile = args.outputfile
     
     AuthorizationManager = AuthorizationManager()
+
+    logger.info(f'Connected to realm {kc_realm}')
 
     if args.checkmode:
         logger.info('Running in checkmode')
         AuthorizationManager.checkmode = True
+
+    available_cmds = ['execute', 'exportcsv', 'export_group_members',
+                      'export_groups_and_roles', 'import_groups_and_roles']
+
+    if not cmd in available_cmds:
+        print(f'Invalid command: {cmd}')
+        print('Available commands: ')
+        for c in available_cmds:
+            print(f'- {c}')
+        sys.exit()
 
     if cmd == 'execute':
         AuthorizationManager.get_to_desired_state(inputfile)
@@ -191,15 +268,12 @@ if __name__ == '__main__':
         group_lookup = AuthorizationManager.get_group_lookup()
         users_current = AuthorizationManager.get_users_current_state(group_lookup)
         AuthorizationManager.export_users_current_csv(users_current)
-    if cmd == 'exportgroup':
+    if cmd == 'export_group_members':
         if not args.name:
             sys.exit('Please provide a group name to export')
-
-        AuthorizationManager.export_group_csv(args.name)
-
-
-
-
-
-
+        AuthorizationManager.export_group_members(args.name)
+    if cmd == 'export_groups_and_roles':
+        AuthorizationManager.export_groups_and_roles(outputfile)
+    if cmd == 'import_groups_and_roles':
+        AuthorizationManager.import_groups_and_roles(inputfile)
 
